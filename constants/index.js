d0f0e6c859cbab3b0452af7ded6b9e1f331fa4fc
BABAK <107608596+mrBABIT@users.noreply.github.com>
2023-07-20 11:35:49 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xffb5674046d71c6d9ec6c2afe4b0cdd0c0f90f41";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
